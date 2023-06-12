66ed13503cf9d94391852adaacf3f8bbe0372e95
astro543 <kinyuab54@gmail.com>
2023-06-12 05:08:44 -0700

    in list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+def replace_in_list(my_list, idx, element):
+    """Replace an element of a lst at a specific position."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return my_list
+
+    my_list[idx] = element
+    return my_list
