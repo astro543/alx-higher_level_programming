9b4c1b7be40d4287f099923d25a06449a084a5cc
astro543 <kinyuab54@gmail.com>
2023-07-10 11:31:29 -0700

    inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Defines an inherited list Class MyList"""
+
+
+class MyList(list):
+    """a subclass of list"""
+
+    def print_sorted(self):
+        """prints the sorted list"""
+        print(sorted(self))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/1-my_list.txt")
---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for MyInt class"""
+
+
+class MyInt(int):
+    """subclass MyInt"""
+
+    def __eq__(self, other):
+        """override equals, inverting it"""
+        return int(self) != int(other)
+
+    def __ne__(self, other):
+        """overrides not_equals, inverting it"""
+        return int(self) == int(other)
