cf8db33ec2f88f679c047f13b0e0f72d88bac4a5
astro543 <kinyuab54@gmail.com>
2023-08-28 14:36:18 -0700

    comedy only

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,9 @@
 -- lists all Comedy shows in the database hbtn_0d_tvshows
-SELECT tv_shows.title
-FROM tv_genres, tv_show_genres, tv_shows
-WHERE tv_genres.name = "Comedy" AND tv_show_genres.genre_id = tv_genres.id AND tv_show_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title ASC;~
+-- lists all rows of a database corresponding to a column value
+SELECT title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+GROUP BY title
+ORDER BY title ASC;
